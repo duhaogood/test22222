b8e715d287b0285c800b4f3a03fd48c844a6cc83
mac_hao <mac_hao@mac-haodeMac-mini.local>
2017-05-22 18:39:31 +0800

    haha

---
+++ test2/test2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test2
+//
+//  Created by mac_hao on 2017/5/22.
+//  Copyright © 2017年 杜浩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ test2/test2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test2
+//
+//  Created by mac_hao on 2017/5/22.
+//  Copyright © 2017年 杜浩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
